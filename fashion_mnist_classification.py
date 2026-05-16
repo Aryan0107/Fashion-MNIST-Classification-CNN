3a7460e457acfad8059becf129ebbbf2cf24d3a9
Aryan Kumar <anshsuroy143@gmail.com>
2026-05-16 17:04:27 +0530

    Create fashion_mnist_classification.py

---
+++ fashion_mnist_classification.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Loading the Fashion MNIST dataset
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+# Class names
+class_names = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot"
+]
+
+# Normalizing pixel values from 0-255 to 0-1
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Reshaping data for CNN
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# Building the CNN model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# Compiling the model
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Training the model
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=5,
+    validation_data=(x_test, y_test)
+)
+
+# Evaluating the model
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+
+print("Test Accuracy:", test_accuracy)
+
+# Making prediction on one test image
+index = 0
+sample_image = x_test[index]
+
+prediction = model.predict(sample_image.reshape(1, 28, 28, 1))
+predicted_class = np.argmax(prediction)
+
+print("Predicted Class:", class_names[predicted_class])
+print("Actual Class:", class_names[y_test[index]])
+
+# Displaying the image
+plt.imshow(sample_image.reshape(28, 28), cmap='gray')
+plt.title(f"Predicted: {class_names[predicted_class]}, Actual: {class_names[y_test[index]]}")
+plt.axis('off')
+plt.show()
+
+# Saving the model
+model.save("fashion_mnist_cnn_model.h5")
